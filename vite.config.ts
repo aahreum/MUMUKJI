560338c6891030396177d8fe39dbdd1fa655f0d6
Ahreum <cocoding420@gmail.com>
2024-02-14 20:38:49 +0900

    🌱 Chore: svg 컴포넌트 사용 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': '/src',
