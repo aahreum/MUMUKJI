b8e40fc2a9e8fe471b04855fb771a779d45d5baf
Ahreum <cocoding420@gmail.com>
2024-02-14 19:42:19 +0900

    🔥 Remove: common.scss import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from 'react-router-dom'
-import './styles/common.scss'
 import { router } from './routers/router'
 
 const App = (): React.ReactElement => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
