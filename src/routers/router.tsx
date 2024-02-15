bf9f541826ea3d4715b57d4383d94d974ccb5f9f
Ahreum <cocoding420@gmail.com>
2024-02-16 00:48:58 +0900

    ✨ Feat: 라우터 설정 추가

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,14 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Index from '@/pages/Index'
 import Edit from '@/pages/Edit'
+import Layout from '@/pages/Layout'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Index />,
-  },
-  {
-    path: 'edit',
-    element: <Edit />,
+    id: 'rootLayout',
+    element: <Layout />,
+    children: [
+      {
+        id: 'index',
+        path: '/',
+        element: <Index />,
+      },
+      {
+        id: 'edit',
+        path: '/edit',
+        element: <Edit />,
+      },
+    ],
   },
 ])
