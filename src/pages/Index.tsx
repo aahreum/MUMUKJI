1d3c5d72ac3878f2b711f6963c00d30db8b5dbcc
Ahreum <cocoding420@gmail.com>
2024-02-16 01:44:56 +0900

    ✨ Feat: 메인 레이아웃 추가

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,8 @@
 import image from '/images/ani.png'
 import styles from '@/styles/pages/index.module.scss'
 import TopLogo from '@/components/common/TopLogo'
+import MainRouletteButton from '@/components/rouletteButton/MainRouletteButton'
+import GroupRouletteList from '@/components/rouletteButton/GroupRouletteList'
 
 const Index = (): React.ReactElement => {
   return (
@@ -8,6 +10,10 @@ const Index = (): React.ReactElement => {
       <TopLogo />
       <main className={styles.container}>
         <img className={styles.image} src={image} alt="메인이미지" />
+        <div className={styles.buttonArea}>
+          <MainRouletteButton />
+          <GroupRouletteList />
+        </div>
       </main>
     </>
   )
