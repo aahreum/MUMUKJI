48497a7751b9f24ec594065420c8705e3864f2bb
Ahreum <cocoding420@gmail.com>
2024-02-16 01:44:44 +0900

    ✨ Feat: 그룹 룰렛 컴포넌트 추가

---
+++ src/components/rouletteButton/GroupRouletteButton.tsx
@@ -0,0 +1,7 @@
+import styles from './groupRouletteButton.module.scss'
+
+const GroupRouletteButton = () => {
+  return <button className={styles.container}>그룹명</button>
+}
+
+export default GroupRouletteButton
