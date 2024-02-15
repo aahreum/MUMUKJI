23675af1a0eeb441f17a9e363c5b03fb789b4321
Ahreum <cocoding420@gmail.com>
2024-02-16 00:49:38 +0900

    ✨ Feat: 탭바 컴포넌트 추가

---
+++ src/components/common/TabBar.tsx
@@ -0,0 +1,41 @@
+import styles from './tabBar.module.scss'
+import TabBarButton from './TabBarButton'
+import RiceIcon from '@/assets/icons/rice.svg?react'
+import HomeIcon from '@/assets/icons/home.svg?react'
+import EditGroupIcon from '@/assets/icons/edit_group.svg?react'
+
+export interface tabMenuProps {
+  icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  menu: string
+  url: string
+}
+
+const TabBar = () => {
+  const tabMenu: tabMenuProps[] = [
+    {
+      icon: RiceIcon,
+      menu: '머먹었더라?',
+      url: '/list',
+    },
+    {
+      icon: HomeIcon,
+      menu: '홈',
+      url: '/',
+    },
+    {
+      icon: EditGroupIcon,
+      menu: '그룹 편집',
+      url: '/edit',
+    },
+  ]
+
+  return (
+    <div className={styles.container}>
+      {tabMenu.map((item) => (
+        <TabBarButton key={item.menu} icon={item.icon} menu={item.menu} url={item.url} />
+      ))}
+    </div>
+  )
+}
+
+export default TabBar
