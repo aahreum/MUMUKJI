42df0686be631582aef308f549904662cb57c1e4
Ahreum <cocoding420@gmail.com>
2024-02-26 21:13:25 +0900

    ✨ Feat: 메뉴 추천 함수 추가

--- src/components/rouletteButton/MainRouletteButton.tsx
+++ src/components/rouletteButton/MainRouletteButton.tsx
@@ -2,8 +2,20 @@ import styles from './mainRouletteButton.module.scss'
 import ArrowRight from '@/assets/icons/arrow_right.svg?react'
 
 const MainRouletteButton = () => {
+  const MENU1 = ['짜장면', '짬뽕', '탕수육', '마라탕', '피자', '치킨']
+  const MENU2 = ['라면', '떡볶이', '샤브샤브']
+
+  const chooseMenu = (...menuArrays: string[][]) => {
+    const combinedMenu = [...menuArrays.flat()]
+    const randomIndex = Math.floor(Math.random() * combinedMenu.length)
+    const choice = combinedMenu[randomIndex]
+
+    console.log(combinedMenu)
+    console.log(choice)
+  }
+
   return (
-    <button className={styles.container} type="button">
+    <button className={styles.container} type="button" onClick={() => chooseMenu(MENU1, MENU2)}>
       <span>
         <span className={styles.accent}>머먹을지</span> 안 정했어?
       </span>
