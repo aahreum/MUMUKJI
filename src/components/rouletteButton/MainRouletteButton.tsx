83dcbba33f535af36a160ef3258cdf41226ab743
Ahreum <cocoding420@gmail.com>
2024-03-16 22:44:12 +0900

    ✨ Feat: 룰렛 모달 추가, 룰렛 코드 삭제

--- src/components/rouletteButton/MainRouletteButton.tsx
+++ src/components/rouletteButton/MainRouletteButton.tsx
@@ -1,29 +1,24 @@
 import styles from './mainRouletteButton.module.scss'
 import ArrowRight from '@/assets/icons/arrow_right.svg?react'
+import ChooseMenuModal from '@/components/chooseMenuModal/ChooseMenuModal'
+import useModal from '@/hooks/useModal'
 
 const MainRouletteButton = () => {
-  const MENU1 = ['짜장면', '짬뽕', '탕수육', '마라탕', '피자', '치킨']
-  const MENU2 = ['라면', '떡볶이', '샤브샤브']
-
-  const chooseMenu = (...menuArrays: string[][]) => {
-    const combinedMenu = [...menuArrays.flat()]
-    const randomIndex = Math.floor(Math.random() * combinedMenu.length)
-    const choice = combinedMenu[randomIndex]
-
-    console.log(combinedMenu)
-    console.log(choice)
-  }
+  const { modalOpen, openModal, closeModal } = useModal()
 
   return (
-    <button className={styles.container} type="button" onClick={() => chooseMenu(MENU1, MENU2)}>
-      <span>
-        <span className={styles.accent}>머먹을지</span> 안 정했어?
-      </span>
-      <div className={styles.rightArea}>
-        <span>먹지추천받기</span>
-        <ArrowRight />
-      </div>
-    </button>
+    <>
+      {modalOpen && <ChooseMenuModal closeModal={closeModal} />}
+      <button className={styles.container} type="button" onClick={openModal}>
+        <span>
+          <span className={styles.accent}>머먹을지</span> 안 정했어?
+        </span>
+        <div className={styles.rightArea}>
+          <span>먹지추천받기</span>
+          <ArrowRight />
+        </div>
+      </button>
+    </>
   )
 }
 
