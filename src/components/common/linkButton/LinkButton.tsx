1ab8d63ac8914d4f372a707190abb3fe57b76db9
Ahreum <cocoding420@gmail.com>
2024-03-22 01:42:58 +0900

    ♻️ Refactor: 폴더명 변경, props 수정

---
+++ src/components/common/linkButton/LinkButton.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import styles from './linkButton.module.scss'
+
+interface linkButtonProps {
+  /** 이동 경로 입력
+   */
+  link: string
+  /** 버튼 타입 선택
+   */
+  primary?: boolean
+  /** 버튼 라벨
+   */
+  label: string
+}
+
+const LinkButton = ({ link, label, primary = true }: linkButtonProps): React.ReactElement => {
+  return (
+    <Link className={`${styles.button} ${primary ? '' : styles.sub}`} to={link}>
+      {label}
+    </Link>
+  )
+}
+
+export default LinkButton
