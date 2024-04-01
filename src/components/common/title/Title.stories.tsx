6aa6d4fa01d359afddea0757bb0d26e8160a5160
Ahreum <cocoding420@gmail.com>
2024-04-02 02:08:00 +0900

    🧪 Story: title 스토리 설명 추가, children 값 변경

--- src/components/common/title/Title.stories.tsx
+++ src/components/common/title/Title.stories.tsx
@@ -4,32 +4,33 @@ import Title from './Title'
 const meta = {
   title: 'Components/common/Title',
   component: Title,
-  decorators: (story) => story(),
+  parameters: {
+    componentSubtitle: '타이틀 컴포넌트는 화면(페이지, 모달 등) 메인 텍스트에 사용합니다.',
+    docs: {
+      description: {
+        component: `<li>children: 타이틀을 입력합니다. (&lt;br /&gt;을 사용하여 줄바꿈이 가능합니다)</li><li>textAlignCenter: 텍스트 정렬 값으로 <span class='css-o1d7ko css-in3yi3'>'true' | 'false'</span> 중 하나를 선택할 수 있습니다.</li>
+        `,
+      },
+    },
+  },
   tags: ['autodocs'],
-
-  args: {},
+  decorators: (story) => <div style={{ width: '500px' }}>{story()}</div>,
 } satisfies Meta<typeof Title>
 
 export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const TitleLeft: Story = {
+export const TitleCenter: Story = {
   args: {
-    children: (
-      <>
-        왼쪽 정렬
-        <br />
-        타이틀
-      </>
-    ),
-    textAlignCenter: false,
+    children: '가운데 정렬 타이틀',
+    textAlignCenter: true,
   },
 }
 
-export const TitleCenter: Story = {
+export const TitleLeft: Story = {
   args: {
-    children: <>가운데 정렬 타이틀</>,
-    textAlignCenter: true,
+    children: '왼쪽 정렬 타이틀',
+    textAlignCenter: false,
   },
 }
