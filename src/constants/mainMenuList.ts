090a05e3577163bfacbfc125ef2d0dd4e17a723b
Ahreum <cocoding420@gmail.com>
2024-04-02 23:28:49 +0900

    ♻️ Refactor: 메인메뉴리스트 상수값 수정, 카테고리 이미지 추가

--- src/constants/mainMenuList.ts
+++ src/constants/mainMenuList.ts
@@ -1,7 +1,57 @@
-const MAIN_MENU_LIST = [
+interface MAIN_MENU_LIST_T {
+  id: number
+  theme: string
+  image: string
+  menu: string[]
+}
+
+const MAIN_MENU_LIST: MAIN_MENU_LIST_T[] = [
   {
     id: 0,
     theme: '한식',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 1,
+    theme: '일식',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 2,
+    theme: '중식',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 3,
+    theme: '양식',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 4,
+    theme: '아시안',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 5,
+    theme: '패스트푸드',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 6,
+    theme: '분식',
+    image: '/images/foodCategory/korean.png',
+    menu: ['갈비탕', '설렁탕'],
+  },
+  {
+    id: 7,
+    theme: '식단조절',
+    image: '/images/foodCategory/korean.png',
     menu: ['갈비탕', '설렁탕'],
   },
 ]
