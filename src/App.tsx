ca38146957abf088c9f75e9152b86724e874285a
Ahreum <cocoding420@gmail.com>
2024-04-03 19:14:53 +0900

    ✨ Feat: RecoilRoot 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router'
 import Wrap from './components/common/wrap/Wrap'
+import { RecoilRoot } from 'recoil'
 
 const App = (): React.ReactElement => {
   return (
-    <Wrap>
-      <RouterProvider router={router} />
-    </Wrap>
+    <RecoilRoot>
+      <Wrap>
+        <RouterProvider router={router} />
+      </Wrap>
+    </RecoilRoot>
   )
 }
 
