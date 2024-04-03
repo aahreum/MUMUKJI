40900f490695506ae393baff71e3627016a5d899
Ahreum <cocoding420@gmail.com>
2024-04-03 17:29:56 +0900

    🐛 Fix: label 오타 수정

--- src/components/common/checkbox/Checkbox.tsx
+++ src/components/common/checkbox/Checkbox.tsx
@@ -2,16 +2,16 @@ import styles from './checkbox.module.scss'
 
 interface CheckBoxProps {
   /** 체크박스 텍스트 입력 */
-  lable: string
+  label: string
   /** 체크박스 체크 여부 */
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const CheckBox = ({ lable, onChange }: CheckBoxProps) => {
+const CheckBox = ({ label, onChange }: CheckBoxProps) => {
   return (
     <label className={styles.container}>
       <input className={styles.checkbox} type="checkbox" onChange={onChange} />
-      <span className={styles.label}>{lable}</span>
+      <span className={styles.label}>{label}</span>
     </label>
   )
 }
