10b7aed4f15803a3e199fc9a2b737e1153e48648
Ahreum <cocoding420@gmail.com>
2024-05-01 00:48:53 +0900

    ✨ Feat: 전체 모달 끄기 함수 추가

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -3,24 +3,31 @@ import { modalCountState } from '@/recoil/modalCount'
 import { useRecoilState } from 'recoil'
 
 const useModal = () => {
-  const [isOpen, setisOpen] = useState(false)
   const [modalCount, setModalCount] = useRecoilState(modalCountState)
+  const [isOpen, setisOpen] = useState(false)
 
   const openModal = () => {
     setModalCount((prev) => prev + 1)
+    console.log(modalCount)
     document.body.style.overflow = 'hidden'
     setisOpen(true)
   }
 
   const closeModal = () => {
     setModalCount((prev) => prev - 1)
+    console.log(modalCount)
     if (modalCount <= 1) {
       document.body.style.overflow = 'auto'
     }
     setisOpen(false)
   }
 
-  return { isOpen, openModal, closeModal }
+  const closeAllModal = () => {
+    setModalCount(0)
+    document.body.style.overflow = 'auto'
+  }
+
+  return { isOpen, openModal, closeModal, closeAllModal }
 }
 
 export default useModal
