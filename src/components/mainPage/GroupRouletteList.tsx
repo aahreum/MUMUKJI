2132c83dfe8c16e06d27505e1ee41b6d4318379e
Ahreum <cocoding420@gmail.com>
2024-05-06 12:43:41 +0900

    ♻️ Refactor: 그룹룰렛리스트 테스트용 컴포넌트 추가

--- src/components/mainPage/GroupRouletteList.tsx
+++ src/components/mainPage/GroupRouletteList.tsx
@@ -1,9 +1,15 @@
-import Button from '@/components/common/button/Button'
+import RouletteGroupButton from '../common/rouletteGroupButton/RouletteGroupButton'
 
 const GroupRouletteList = () => {
   return (
     <>
-      <Button label="그룹명" primary={false} light={true} />
+      <RouletteGroupButton
+        primary={false}
+        title="서울 사무실 점심"
+        desc="홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이, 홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이"
+        onClick={() => console.log('하하')}
+        rightText={null}
+      />
     </>
   )
 }
