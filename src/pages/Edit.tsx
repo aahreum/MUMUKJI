fea149d1be6f12a88a0678479849d462267efe7c
Ahreum <cocoding420@gmail.com>
2024-06-21 00:39:09 +0900

    ✨ Feat: 그룹 편집 빈값 레이아웃 추가

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,11 +1,21 @@
+import styles from '@/styles/pages/edit.module.scss'
+import Button from '@/components/common/button/Button'
 import Header from '@/components/common/header/Header'
+import emptyImg from '/images/empty.png'
 
 const Edit = () => {
   return (
-    <div>
+    <>
       <Header title="그룹 편집" />
-      <p>그룹편집페이지</p>
-    </div>
+      <div className={styles.emptyContainer}>
+        <div className={styles.emptyContentsArea}>
+          <img src={emptyImg} alt="emptyImg" />
+          <p className={styles.title}>저장된 그룹이 없어요</p>
+          <p className={styles.desc}>나만의 메뉴로 구성된 그룹을 생성해보세요.</p>
+        </div>
+        <Button label="그룹 추가하기" type="button" onClick={() => {}} />
+      </div>
+    </>
   )
 }
 
