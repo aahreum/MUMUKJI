78b6862e8a966704a44b2f8b2a2f19d164944901
Ahreum <cocoding420@gmail.com>
2024-07-10 15:47:31 +0900

    💄 Style: secondary 추가, css 이름 변경

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -5,13 +5,19 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
   /** 버튼 스타일 */
   primary?: boolean
+  secondary?: boolean
   /** 버튼 폰트 스타일 */
   light?: boolean
 }
 
-const Button = ({ type = 'button', label, onClick, primary = true, disabled = false, light = false }: ButtonProps) => {
+const Button = ({ type = 'button', label, onClick, primary = true, secondary = false, disabled = false, light = false }: ButtonProps) => {
   return (
-    <button disabled={disabled} className={`${styles.button} ${primary ? '' : styles.sub} ${light ? styles.light : ''}`} type={type} onClick={onClick}>
+    <button
+      disabled={disabled}
+      className={`${styles.button} ${primary ? '' : secondary ? styles.secondary : styles.basic} ${light ? styles.light : ''}`}
+      type={type}
+      onClick={onClick}
+    >
       {label}
     </button>
   )
