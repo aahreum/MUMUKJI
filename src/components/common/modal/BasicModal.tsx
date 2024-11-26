ffe95e555f453f7df4342fe863d55c6e4e262309
Ahreum <cocoding420@gmail.com>
2024-11-26 20:13:57 +0900

    ♻️ Refactor: children 파라미터 타입 정의 변경

--- src/components/common/modal/BasicModal.tsx
+++ src/components/common/modal/BasicModal.tsx
@@ -18,19 +18,17 @@ interface BasicModalProps {
 const BasicModal = ({ title, desc, firstBtnColor, firstBtnLabel, firstBtnOnClick, secondBtnColor, secondBtnLabel, secondBtnOnClick }: BasicModalProps) => {
   return (
     <ModalPotal>
-      <>
-        <ModalBG />
-        <div className={styles.container}>
-          <div className={styles.textArea}>
-            <p className={styles.title}>{title}</p>
-            <p className={styles.desc}>{desc}</p>
-          </div>
-          <div className={styles.buttonArea}>
-            <Button color={firstBtnColor} size="m" label={firstBtnLabel} onClick={firstBtnOnClick} />
-            {secondBtnLabel && <Button color={secondBtnColor} size="m" label={secondBtnLabel} onClick={secondBtnOnClick} />}
-          </div>
+      <ModalBG />
+      <div className={styles.container}>
+        <div className={styles.textArea}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.desc}>{desc}</p>
         </div>
-      </>
+        <div className={styles.buttonArea}>
+          <Button color={firstBtnColor} size="m" label={firstBtnLabel} onClick={firstBtnOnClick} />
+          {secondBtnLabel && <Button color={secondBtnColor} size="m" label={secondBtnLabel} onClick={secondBtnOnClick} />}
+        </div>
+      </div>
     </ModalPotal>
   )
 }
