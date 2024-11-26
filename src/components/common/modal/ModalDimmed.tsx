35fb68f6c0cc9c5edcea32269809794baf42d6e2
Ahreum <cocoding420@gmail.com>
2024-11-26 20:27:55 +0900

    🔥 Remove: onclick 파라미터 삭제

--- src/components/common/modal/ModalDimmed.tsx
+++ src/components/common/modal/ModalDimmed.tsx
@@ -1,7 +1,7 @@
 import styles from './modalDimmed.module.scss'
 
-const ModalDimmed = ({ onClick }: { onClick?: () => void }) => {
-  return <div className={styles.container} onClick={onClick} />
+const ModalDimmed = () => {
+  return <div className={styles.container} />
 }
 
 export default ModalDimmed
