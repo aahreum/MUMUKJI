add85fd377876453ed820696d163993c50fe619b
Ahreum <cocoding420@gmail.com>
2025-01-22 00:27:22 +0900

    ♻️ Refactor: 텍스트필드 key값 받도록 수정

--- src/hooks/useTextfield.tsx
+++ src/hooks/useTextfield.tsx
@@ -1,11 +1,13 @@
 import { useRecoilState } from 'recoil'
 import { textfieldData } from '@/recoil/textfieldData'
 
-const useTextfield = () => {
-  const [textfield, setTextfield] = useRecoilState(textfieldData)
+const useTextfield = (key: string) => {
+  const [textfield, setTextfield] = useRecoilState(textfieldData(key))
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTextfield(e.target.value)
   }
+
   const handleInputDelete = () => {
     setTextfield('')
   }
