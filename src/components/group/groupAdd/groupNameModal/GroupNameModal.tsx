35ec3ff6d8eb53455e1d1b0f714487927afbda29
Ahreum <cocoding420@gmail.com>
2025-01-22 01:14:19 +0900

    ✨ Feat: 그룹 이름 변경 모달 추가

---
+++ src/components/group/groupAdd/groupNameModal/GroupNameModal.tsx
@@ -0,0 +1,48 @@
+import styles from './groupNameModal.module.scss'
+import useTextfield from '@/hooks/useTextfield'
+import ModalPotal from '@/components/common/modal/ModalPotal'
+import ModalDimmed from '@/components/common/modal/ModalDimmed'
+import Textfield from '@/components/common/textfield/Textfield'
+import Button from '@/components/common/button/Button'
+
+interface groupNameModalProps {
+  closeModal: () => void
+}
+
+const GroupNameModal = ({ closeModal }: groupNameModalProps) => {
+  const { textfield, handleInputChange, handleInputDelete } = useTextfield('groupName')
+  return (
+    <ModalPotal>
+      <ModalDimmed />
+      <div className={styles.container}>
+        <p className={styles.title}>그룹이름</p>
+        <Textfield styleType="outline" placeholder="그룹명을 입력하세요." value={textfield} onChange={handleInputChange} handleInputDelete={handleInputDelete} />
+        <div className={styles.buttonArea}>
+          <Button
+            label="닫기"
+            styleType="outline"
+            roundType="square"
+            color="tertiary"
+            size="s"
+            onClick={() => {
+              handleInputDelete(), closeModal()
+            }}
+          />
+          <Button
+            label="완료"
+            styleType="solid"
+            roundType="square"
+            color="secondary"
+            size="s"
+            disabled={textfield.length === 0}
+            onClick={() => {
+              handleInputDelete(), closeModal()
+            }}
+          />
+        </div>
+      </div>
+    </ModalPotal>
+  )
+}
+
+export default GroupNameModal
