18debb99cc17e6be3f4d9818f033813393f2e1fa
Ahreum <cocoding420@gmail.com>
2025-01-21 22:37:48 +0900

    🧪 Story: 링크버튼 설명 수정, 피그마 링크 추가, 예시 테스트 변경, argTypes 추가

--- src/components/common/linkButton/LinkButton.stories.tsx
+++ src/components/common/linkButton/LinkButton.stories.tsx
@@ -5,21 +5,41 @@ import { BrowserRouter } from 'react-router-dom'
 const meta = {
   title: 'Components/common/LinkButton',
   component: LinkButton,
+  tags: ['autodocs'],
   parameters: {
-    componentSubtitle: '링크 버튼 컴포넌트는 페이지 이동 시 사용하는 버튼입니다.',
+    componentSubtitle: '머먹지에서 페이지 이동이 있을때 사용하는 링크 버튼 컴포넌트입니다.',
     docs: {
       description: {
-        component: `<li>link: 이동할 경로를 입력합니다.</li><li>label: 버튼 텍스트를 입력합니다.</li><li>primary: 버튼 스타일 값으로 <span className='css-o1d7ko css-in3yi3'>'true' | 'false'</span> 중 하나를 선택할 수 있습니다.</li>
-        `,
+        component: '<li><code>link</code><code>label</code>은 필수로 입력해야 합니다.</li>',
       },
     },
   },
-  decorators: (story) => (
+
+  argTypes: {
+    link: {
+      description: '이동 경로',
+      table: {
+        category: 'required',
+      },
+    },
+    label: {
+      description: '버튼 라벨',
+      table: {
+        category: 'required',
+      },
+    },
+    primary: {
+      description: 'primary style 적용 여부',
+      table: {
+        category: 'optional',
+      },
+    },
+  },
+  decorators: (Story) => (
     <BrowserRouter>
-      <div style={{ width: '600px' }}>{story()}</div>
+      <div style={{ width: '600px' }}>{<Story />}</div>
     </BrowserRouter>
   ),
-  tags: ['autodocs'],
 } satisfies Meta<typeof LinkButton>
 
 export default meta
@@ -29,15 +49,27 @@ type Story = StoryObj<typeof LinkButton>
 export const Primary: Story = {
   args: {
     link: '/mumukji',
-    label: '페이지 이동 버튼',
+    label: '홈으로 돌아가기',
     primary: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1780-6938&t=pD666ta77og4q7AJ-1',
+    },
   },
 }
 
 export const Secondary: Story = {
   args: {
     link: '/mumukji',
-    label: '페이지 이동 버튼',
+    label: '홈으로 돌아가기',
     primary: false,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1780-6938&t=pD666ta77og4q7AJ-1',
+    },
   },
 }
