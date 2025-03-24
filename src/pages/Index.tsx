3b561e1fcace42ea2ea7e725bdf6c4bfac6581da
ahreum <cocoding420@gmail.com>
2025-03-24 11:49:30 +0900

    ♻️ Refactor: 링크 버튼 컴포넌트로 수정, 버튼 스타일 추가

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,13 @@
 import styles from '@/styles/pages/index.module.scss'
 import useModal from '@/hooks/useModal'
+import { useRecoilValue } from 'recoil'
+import { modalCountState } from '@/recoil/modalCount'
+import AddIcon from '@/assets/icons/add.svg?react'
 import TopLogo from '@/components/common/topLogo/TopLogo'
 import RouletteGroupButton from '@/components/mainPage/rouletteGroupButton/RouletteGroupButton'
 import GroupRouletteList from '@/components/mainPage/groupRouletteList/GroupRouletteList'
 import ChooseMenuModal from '@/components/chooseMenuModal/ChooseMenuModal'
-import { useRecoilValue } from 'recoil'
-import { modalCountState } from '@/recoil/modalCount'
-import GroupAddButton from '@/components/mainPage/groupAddButton/GroupAddButton'
+import LinkButton from '@/components/common/linkButton/LinkButton'
 
 const Index = (): React.ReactElement => {
   const { isOpen, openModal, closeModal } = useModal()
@@ -22,7 +23,7 @@ const Index = (): React.ReactElement => {
           <GroupRouletteList />
         </div>
       </main>
-      <GroupAddButton />
+      <LinkButton className={styles.groupAddButton} label={'그룹추가'} link={'/group/add'} size="m" icon={<AddIcon />} roundType="capsule" full={false} />
     </>
   )
 }
