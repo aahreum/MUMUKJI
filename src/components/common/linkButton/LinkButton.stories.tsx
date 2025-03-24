254539e900889df2b92ece6842040fd9aafcb70e
ahreum <cocoding420@gmail.com>
2025-03-24 11:29:06 +0900

    🧪 Story: 링크 버튼 스토리 변경

--- src/components/common/linkButton/LinkButton.stories.tsx
+++ src/components/common/linkButton/LinkButton.stories.tsx
@@ -7,7 +7,7 @@ const meta = {
   component: LinkButton,
   tags: ['autodocs'],
   parameters: {
-    componentSubtitle: '머먹지에서 페이지 이동이 있을때 사용하는 링크 버튼 컴포넌트입니다.',
+    componentSubtitle: '머먹지에서 페이지를 이동할때 사용하는 링크 버튼 컴포넌트입니다.',
     docs: {
       description: {
         component: '<li><code>link</code><code>label</code>은 필수로 입력해야 합니다.</li>',
@@ -28,8 +28,40 @@ const meta = {
         category: 'required',
       },
     },
-    primary: {
-      description: 'primary style 적용 여부',
+    styleType: {
+      description: '버튼 스타일',
+      table: {
+        category: 'optional',
+      },
+    },
+    roundType: {
+      description: '버튼 라운드 스타일',
+      table: {
+        category: 'optional',
+      },
+    },
+    size: {
+      description: '버튼 크기',
+      table: {
+        category: 'optional',
+      },
+    },
+    color: {
+      description: '버튼 컬러',
+      table: {
+        category: 'optional',
+      },
+    },
+    icon: {
+      control: 'text',
+      description: '아이콘',
+      table: {
+        category: 'optional',
+        type: { summary: 'ReactNode' },
+      },
+    },
+    full: {
+      description: '풀 사이즈 버튼 여부',
       table: {
         category: 'optional',
       },
@@ -37,7 +69,7 @@ const meta = {
   },
   decorators: (Story) => (
     <BrowserRouter>
-      <div style={{ width: '600px' }}>{<Story />}</div>
+      <div style={{ width: '600px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>{<Story />}</div>
     </BrowserRouter>
   ),
 } satisfies Meta<typeof LinkButton>
@@ -46,11 +78,15 @@ export default meta
 
 type Story = StoryObj<typeof LinkButton>
 
-export const Primary: Story = {
+export const SolidPrimary: Story = {
   args: {
     link: '/mumukji',
     label: '홈으로 돌아가기',
-    primary: true,
+    styleType: 'solid',
+    roundType: 'loundSquare',
+    color: 'primary',
+    size: 'l',
+    full: true,
   },
   parameters: {
     design: {
@@ -60,16 +96,128 @@ export const Primary: Story = {
   },
 }
 
-export const Secondary: Story = {
+export const SolidSecondary: Story = {
   args: {
     link: '/mumukji',
     label: '홈으로 돌아가기',
-    primary: false,
+    styleType: 'solid',
+    roundType: 'loundSquare',
+    color: 'secondary',
+    size: 'l',
+    full: true,
   },
   parameters: {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1780-6938&t=pD666ta77og4q7AJ-1',
+    },
+  },
+}
+
+export const SolidTertiary: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'solid',
+    roundType: 'loundSquare',
+    color: 'tertiary',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
+    },
+  },
+}
+
+export const SolidNegative: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'solid',
+    roundType: 'loundSquare',
+    color: 'negative',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
+    },
+  },
+}
+
+export const OutlinePrimary: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'outline',
+    roundType: 'loundSquare',
+    color: 'primary',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
+    },
+  },
+}
+
+export const OutlineSecondary: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'outline',
+    roundType: 'loundSquare',
+    color: 'secondary',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
+    },
+  },
+}
+
+export const OutlineTertiary: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'outline',
+    roundType: 'loundSquare',
+    color: 'tertiary',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
+    },
+  },
+}
+
+export const OutlineNegative: Story = {
+  args: {
+    link: '/mumukji',
+    label: '홈으로 돌아가기',
+    styleType: 'outline',
+    roundType: 'loundSquare',
+    color: 'negative',
+    size: 'l',
+    full: true,
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/3JlzBW0AXeJyALzwGYiyDf/0.1?node-id=1183-863&t=ORHh7KkT6wejj3uq-1',
     },
   },
 }
