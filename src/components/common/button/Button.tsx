b8d1295a3d92323fad8b987c5030eadbd2ae5dc8
ahreum <cocoding420@gmail.com>
2025-03-24 11:21:27 +0900

    💄 Style: scss 파일 재연결

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,4 +1,4 @@
-import styles from './button.module.scss'
+import styles from '@/styles/components/button.module.scss'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
