81e09f1a65144e3ba4de51667ce2e366f7678dd5
ahreum <cocoding420@gmail.com>
2025-04-25 18:33:17 +0900

    ✨ Feat: 그룹 추가 header 컴포넌트 추가

---
+++ src/components/group/groupAdd/GroupAddHeader/GroupAddHeader.tsx
@@ -0,0 +1,39 @@
+import styles from './groupAddHeader.module.scss'
+import { useNavigate } from 'react-router'
+import useModal from '@/hooks/useModal'
+import useGroup from '@/hooks/useGroup'
+import EditIcon from '@/assets/icons/edit.svg?react'
+import GroupNameModal from '@/components/group/groupAdd/groupNameModal/GroupNameModal'
+
+const GroupAddHeader = () => {
+  const navigate = useNavigate()
+  const { isOpen, openModal, closeModal } = useModal()
+  const { groupName, updateGroupName, saveDataToLocalStorage } = useGroup()
+
+  const handleBackButton = () => {
+    if (window.history.length <= 2) {
+      navigate('/')
+    } else {
+      navigate(-1)
+    }
+  }
+  return (
+    <>
+      {isOpen && <GroupNameModal closeModal={closeModal} updateGroupName={updateGroupName} />}
+      <div className={styles.container}>
+        <button className={styles.controlButton} onClick={handleBackButton}>
+          취소
+        </button>
+        <button className={styles.editButton} onClick={openModal}>
+          {groupName}
+          <EditIcon />
+        </button>
+        <button className={`${styles.controlButton} ${styles.saveButton}`} disabled onClick={saveDataToLocalStorage}>
+          저장
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default GroupAddHeader
