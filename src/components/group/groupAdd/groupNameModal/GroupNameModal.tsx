7f45654b90f2bcefdcfd778e594a9fc4c7e4f98b
ahreum <cocoding420@gmail.com>
2025-04-25 18:26:51 +0900

    ✨ Feat: 그룹 이름 변경 로직 추가

--- src/components/group/groupAdd/groupNameModal/GroupNameModal.tsx
+++ src/components/group/groupAdd/groupNameModal/GroupNameModal.tsx
@@ -7,10 +7,22 @@ import Button from '@/components/common/button/Button'
 
 interface groupNameModalProps {
   closeModal: () => void
+  updateGroupName: (newName: string) => void
 }
 
-const GroupNameModal = ({ closeModal }: groupNameModalProps) => {
+const GroupNameModal = ({ closeModal, updateGroupName }: groupNameModalProps) => {
   const { textfield, handleInputChange, handleInputDelete } = useTextfield('groupName')
+
+  const handleCloseModal = () => {
+    handleInputDelete()
+    closeModal()
+  }
+  const handleSaveGroupName = () => {
+    updateGroupName(textfield)
+    handleInputDelete()
+    closeModal()
+  }
+
   return (
     <ModalPotal>
       <ModalDimmed />
@@ -18,27 +30,8 @@ const GroupNameModal = ({ closeModal }: groupNameModalProps) => {
         <p className={styles.title}>그룹이름</p>
         <Textfield styleType="outline" placeholder="그룹명을 입력하세요." value={textfield} onChange={handleInputChange} handleInputDelete={handleInputDelete} />
         <div className={styles.buttonArea}>
-          <Button
-            label="닫기"
-            styleType="outline"
-            roundType="square"
-            color="tertiary"
-            size="s"
-            onClick={() => {
-              handleInputDelete(), closeModal()
-            }}
-          />
-          <Button
-            label="완료"
-            styleType="solid"
-            roundType="square"
-            color="secondary"
-            size="s"
-            disabled={textfield.length === 0}
-            onClick={() => {
-              handleInputDelete(), closeModal()
-            }}
-          />
+          <Button label="닫기" styleType="outline" roundType="square" color="tertiary" size="s" onClick={handleCloseModal} />
+          <Button label="완료" styleType="solid" roundType="square" color="secondary" size="s" disabled={textfield.length === 0} onClick={handleSaveGroupName} />
         </div>
       </div>
     </ModalPotal>
