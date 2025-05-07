b574a1293056359a78b3d62a551c340b2a771911
ahreum <cocoding420@gmail.com>
2025-05-07 19:50:22 +0900

    ✨ Feat: 같은 이름 에러, 메뉴 최대 제한 추가

--- src/components/group/groupAdd/groupAddForm/GroupAddForm.tsx
+++ src/components/group/groupAdd/groupAddForm/GroupAddForm.tsx
@@ -1,7 +1,8 @@
 import styles from './groupAddForm.module.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import useGroup from '@/hooks/useGroup'
 import AddIcon from '@/assets/icons/add.svg?react'
+import CheckIcon from '@/assets/icons/check.svg?react'
 import Button from '@/components/common/button/Button'
 import SelectedBox from '@/components/group/groupAdd/selectedBox/SelectedBox'
 import useTextfield from '@/hooks/useTextfield'
@@ -12,16 +13,34 @@ const GroupAddForm = () => {
   const [selectedItem, setSelectedItem] = useState<string>('')
   const { addMenu, menuList } = useGroup()
   const [showGroupAddForm, setShowGroupAddForm] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    if (menuList.length === 20) {
+      setShowGroupAddForm(false)
+    }
+  }, [menuList])
 
   const formatGroupNumber = (id: number) => {
     return `${id < 10 ? `0${id}` : id}`
   }
 
-  const handleSaveMenu = () => {
-    const newItem = { id: Date.now(), theme: selectedItem, menu: textfield }
-    addMenu(newItem)
+  const handleSaveMenu = (e: React.FormEvent) => {
+    e.preventDefault()
+    const result = addMenu({ id: Date.now(), theme: selectedItem, menu: textfield })
+
+    if (result) {
+      setError(result)
+    } else {
+      setError('')
+      handleInputDelete()
+      setSelectedItem('')
+    }
+  }
+
+  const handleTextfieldDelete = () => {
+    setError('')
     handleInputDelete()
-    setSelectedItem('')
   }
 
   return (
@@ -31,27 +50,40 @@ const GroupAddForm = () => {
           <span>메뉴 목록</span>
           <span className={styles.menuListCountAcc}>{formatGroupNumber(menuList.length)}</span>
         </div>
-        <Button
-          icon={<AddIcon width={16} height={16} />}
-          label="메뉴 추가"
-          color="secondary"
-          disabled={menuList.length === 0 && showGroupAddForm === true}
-          size="s"
-          roundType="square"
-          onClick={() => {
-            setShowGroupAddForm(true)
-          }}
-        />
+        {menuList.length === 20 ? (
+          <div className={styles.finishMsg}>
+            <CheckIcon />
+            20개 완료
+          </div>
+        ) : (
+          <Button
+            icon={<AddIcon width={16} height={16} />}
+            label="메뉴 추가"
+            color="secondary"
+            disabled={showGroupAddForm === true}
+            size="s"
+            roundType="square"
+            onClick={() => {
+              setShowGroupAddForm(true)
+              handleInputDelete()
+              setSelectedItem('')
+              setError('')
+            }}
+          />
+        )}
       </div>
       {showGroupAddForm && (
-        <div className={styles.formContainer}>
+        <form className={styles.formContainer} onSubmit={handleSaveMenu}>
           <SelectedBox selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
-          <Textfield styleType="underline" placeholder="가게 또는 메뉴를 입력해주세요." value={textfield} onChange={handleInputChange} handleInputDelete={handleInputDelete} />
-          <div className={styles.buttonArea}>
-            {menuList.length > 0 && <Button label="취소" styleType="outline" roundType="square" size="xs" color="tertiary" onClick={() => setShowGroupAddForm(false)} />}
-            <Button label="완료" roundType="square" size="xs" color="secondary" disabled={selectedItem === '' || textfield.length === 0} onClick={handleSaveMenu} />
+          <Textfield styleType="underline" placeholder="가게 또는 메뉴를 입력해주세요." value={textfield} onChange={handleInputChange} handleInputDelete={handleTextfieldDelete} />
+          <div className={styles.formBottomArea}>
+            {error && <p className={styles.formErrorMsg}>{error}</p>}
+            <div className={styles.buttonArea}>
+              {menuList.length > 0 && <Button label="취소" styleType="outline" roundType="square" size="xs" color="tertiary" onClick={() => setShowGroupAddForm(false)} />}
+              <Button type="submit" label="완료" roundType="square" size="xs" color="secondary" disabled={selectedItem === '' || textfield.length === 0} />
+            </div>
           </div>
-        </div>
+        </form>
       )}
     </>
   )
