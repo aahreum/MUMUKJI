246b83ffdf0d0c7bb9e8f30cbdec9475550ca0e7
ahreum <cocoding420@gmail.com>
2025-05-07 17:15:50 +0900

    ✨ Feat: 그룹 개수 추가

--- src/components/group/groupList/GroupList.tsx
+++ src/components/group/groupList/GroupList.tsx
@@ -1,10 +1,13 @@
-import { menuListDataTypes } from '@/types/groupDataTypes'
 import styles from './groupList.module.scss'
+import { menuListDataTypes } from '@/types/groupDataTypes'
 import GroupListCard from '@/components/group/groupList/groupListCard/GroupListCard'
 
 const GroupList = ({ data }: { data: menuListDataTypes[] }) => {
   return (
     <div className={styles.container}>
+      <p className={styles.groupLength}>
+        그룹 <span className={styles.acc}>{data.length}</span>
+      </p>
       {data.map((item: menuListDataTypes) => (
         <GroupListCard key={item.groupId} groupName={item.groupName} menu={item.newItem} favorite={item.favorite} />
       ))}
