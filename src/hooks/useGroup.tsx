a0b97d29f8a5bb886ede3402cd7ad1b87d7db134
ahreum <cocoding420@gmail.com>
2025-05-07 19:50:48 +0900

    ♻️ Refactor: useGroupContext로 변경

--- src/hooks/useGroup.tsx
+++ src/hooks/useGroup.tsx
@@ -1,44 +1,7 @@
-import { useNavigate, useParams } from 'react-router-dom'
-import { newItemDataTypes } from '@/types/groupDataTypes'
-import { useEffect, useState } from 'react'
+import { useGroupContext } from '@/contexts/useGroupContext'
 
 const useGroup = () => {
-  const navigate = useNavigate()
-  const { id } = useParams()
-  const urlGroupId = Number(id)
-  const [groupId, setGroupId] = useState<number>(urlGroupId || 0)
-
-  useEffect(() => {
-    if (!urlGroupId) {
-      const storedData = localStorage.getItem('menuList')
-      const storedGroups = storedData ? JSON.parse(storedData) : {}
-      const groupIds = Object.keys(storedGroups).map(Number)
-
-      const maxGroupId = groupIds.length > 0 ? Math.max(...groupIds) : 0
-      setGroupId(maxGroupId)
-    }
-  }, [urlGroupId])
-
-  const addGroup = () => {
-    const newGroupId = groupId + 1
-    setGroupId(newGroupId)
-    navigate(`/group/${newGroupId}`)
-  }
-
-  const saveMenu = ({ id, theme, menu }: newItemDataTypes) => {
-    const storedData = localStorage.getItem('menuList')
-    const storedGroups = storedData ? JSON.parse(storedData) : {}
-
-    if (!storedGroups[urlGroupId]) {
-      storedGroups[urlGroupId] = { menu: [], favorite: false }
-    }
-
-    storedGroups[urlGroupId].menu.push({ id, theme, menu })
-
-    localStorage.setItem('menuList', JSON.stringify(storedGroups))
-  }
-
-  return { groupId, addGroup, saveMenu }
+  return useGroupContext()
 }
 
 export default useGroup
