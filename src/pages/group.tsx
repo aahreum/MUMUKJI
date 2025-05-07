bc59eb316b2f9a3359f9e88bd7a5993d82137356
ahreum <cocoding420@gmail.com>
2025-05-07 17:15:11 +0900

    ♻️ Refactor: 그룹 데이터 로컬스토리지에서 불러오도록 변경

--- src/pages/group.tsx
+++ src/pages/group.tsx
@@ -1,25 +1,34 @@
+import { menuListDataTypes, storedGroupData } from '@/types/groupDataTypes'
 import Header from '@/components/common/header/Header'
-import GroupAddForm from '@/components/group/groupAdd/groupAddForm/GroupAddForm'
+import GroupAddButton from '@/components/group/groupAdd/groupAddButton/GroupAddButton'
 import GroupList from '@/components/group/groupList/GroupList'
 import GroupListEmpty from '@/components/group/groupList/groupListEmpty/GroupListEmpty'
-import { groupDataTypes } from '@/types/groupDataTypes'
 
 const Group = () => {
-  const data: groupDataTypes[] = [
-    { id: 0, title: '서울 사무실 점심', menu: '홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이, 홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이', favorites: true },
-    { id: 1, title: '자취집 만원 이하 식당', menu: '홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이, 홍콩반점, 김밥천국, 왕족발보쌈, 신전떡볶이', favorites: false },
-  ]
+  const rawData = localStorage.getItem('menuList')
+  let dataArray: menuListDataTypes[] = []
+
+  if (rawData) {
+    const parsedData: Record<string, storedGroupData> = JSON.parse(rawData)
+    dataArray = Object.entries(parsedData).map(([key, value]) => ({
+      groupId: parseInt(key),
+      groupName: value.groupName,
+      favorite: value.favorite,
+      newItem: value.menu,
+    }))
+  }
+
   return (
     <>
       <Header title="그룹 목록" />
-      {data.length === 0 ? (
+      {dataArray.length === 0 ? (
         <GroupListEmpty />
       ) : (
         <>
-          <GroupList data={data} />
-          <GroupAddForm />
+          <GroupList data={dataArray} />
         </>
       )}
+      <GroupAddButton />
     </>
   )
 }
