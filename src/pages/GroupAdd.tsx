fa24bd20bc4e8522be2a13465a666b4b6b2a5a6a
ahreum <cocoding420@gmail.com>
2025-05-08 10:34:34 +0900

    ♻️ Refactor: 새로고침, 뒤로가기 방지 코드 개선

--- src/pages/GroupAdd.tsx
+++ src/pages/GroupAdd.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import useGroup from '@/hooks/useGroup'
 import GroupAddHeader from '@/components/group/groupAdd/GroupAddHeader/GroupAddHeader'
 import GroupAddForm from '@/components/group/groupAdd/groupAddForm/GroupAddForm'
@@ -6,14 +6,24 @@ import MenuList from '@/components/group/groupAdd/menuList/MenuList'
 import usePrompt from '@/hooks/usePrompt'
 
 const GroupAdd = () => {
-  const { groupName, menuList, resetData } = useGroup()
-  const initialGroupNameRef = useRef(groupName)
+  const { groupName, menuList, resetData, isSaving, groupId } = useGroup()
+  const initialGroupNameRef = useRef('')
+
+  useEffect(() => {
+    if (groupId && initialGroupNameRef.current === '') {
+      initialGroupNameRef.current = groupName
+    }
+  }, [groupId, groupName])
+
+  const isInitialLoading = useMemo(() => {
+    return !groupId || !groupName || initialGroupNameRef.current === ''
+  }, [groupId, groupName])
 
   const isDirty = useMemo(() => {
     return groupName !== initialGroupNameRef.current || menuList.length > 0
   }, [groupName, menuList])
 
-  usePrompt(isDirty, '변경된 내용이 있습니다. 정말로 나가시겠습니까?', resetData)
+  usePrompt(!isInitialLoading && isDirty && !isSaving, '변경된 내용이 있습니다. 정말로 나가시겠습니까?', resetData)
 
   return (
     <>
